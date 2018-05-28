7e67a189f2c7cf37b417614b504d53a32d09918a
Yorick Phoenix <yp+github@scribblings.com>
2018-05-28 12:40:47 -0700

    Add Privacy Policy for GDPR

--- index.php
+++ index.php
@@ -71,6 +71,7 @@ echo '<button id="loadmore" type="button">Load more</button>';
 //	 '</iframe>';
 
 ?>
+<a href="https://www.iubenda.com/privacy-policy/94703213" class="iubenda-white iubenda-embed " title="Privacy Policy">Privacy Policy</a> <script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
 </body>
 <script type="text/javascript" src="jquery-3.2.1.min.js"></script>
 <script type="text/javascript" src="likesupernice.js"></script>
